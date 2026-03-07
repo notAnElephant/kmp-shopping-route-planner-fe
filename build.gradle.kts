ca370d10ceebc95faa3fde2dda9628ec4eef9f55
Olivér Raisz <raiszoliver@gmail.com>
2026-03-07 17:21:59 +0100

    chore: added ktlint
    also some editorconfig rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.gradle.api.artifacts.VersionCatalogsExtension
+
 plugins {
+    alias(libs.plugins.ktlint)
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
@@ -9,4 +13,21 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinSerialization) apply false
 //    alias(libs.plugins.google.services) apply false
+}
+
+val libsCatalog = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<KtlintExtension> {
+        version.set("1.8.0")
+    }
+
+    dependencies {
+        add(
+            "ktlintRuleset",
+            "io.nlopez.compose.rules:ktlint:${libsCatalog.findVersion("composeRulesKtlint").get().requiredVersion}",
+        )
+    }
 }
